27fa50b3b21da9ae062af98e39c0141cdf602659
Tancy <kchui2008@gmail.com>
2022-07-18 10:23:17 +1000

    add functions to document

---
+++ example.py
@@ -0,0 +1,20 @@
+def func1(arg1, arg2):
+    """
+    this function takes two arguments: set first to equal the second, then returns new first argument.
+
+    :param arg1: some value
+    :param arg2: another value
+
+    :return    : arg1
+    """
+    arg1 = arg2
+    return arg1
+
+def func2():
+    """
+    function without no arguments return None
+
+    :return: None 
+    """
+    return None
+    
